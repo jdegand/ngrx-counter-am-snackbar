e26049258e80ec3ded91cf2a6abc527db418ac7c
jdegand <70610011+jdegand@users.noreply.github.com>
2023-10-14 15:52:10 -0400

    refactor: converted to standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from "@angular/core";
+import { MyCounterComponent } from "./my-counter/my-counter.component";
 
 @Component({
-  selector: 'app-root',
-  template: `
+    selector: 'app-root',
+    template: `
     <h1>NgRx Counter Angular Material Snackbar</h1>
     <app-my-counter></app-my-counter>
-  `
+  `,
+    standalone: true,
+    imports: [MyCounterComponent]
 })
 export class AppComponent {
 
